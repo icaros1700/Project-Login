2df4d99f327293732b13df31fda5d9203b9dc10c
William Ruiz <william.a23@gmail.com>
2019-11-25 13:59:27 -0500

    conexion del registro con la base de datos

--- singup.php
+++ singup.php
@@ -2,7 +2,23 @@
 
 require 'database.php';
 
+$message = '';
+
 if (!empty($_POST['email']) && !empty($_POST['password'])) {
+
+    $sql= "INSERT INTO users (email, password) VALUES (:email, :password)";
+    $stmt= $conn->prepare($sql);
+    $stmt->bindParam(':email',$_POST['email']);
+    $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+    $stmt->bindParam(':password', $password);
+
+    if ($stmt->execute()) {
+
+        $message = 'Successfully Created New User';
+    } else {
+
+        $message = 'Sorry there must have an issue creating your account';
+    }
     
 }
 
@@ -19,6 +35,10 @@ if (!empty($_POST['email']) && !empty($_POST['password'])) {
 <body>
 
 <?php require 'partials/header.php'?>
+
+<?php if (!empty($message)): ?>
+    <p> <?= $message ?></p>
+<?php endif; ?>    
 
 <h1>Sing Up</h1>
 <span>or<a href="login.php"> Login</a></span>
