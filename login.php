a4a46995cf27a7ce7fbf8ea4388d83d52de9273a
William Ruiz <william.a23@gmail.com>
2019-11-25 14:15:18 -0500

    conexion de la pagina login con los datos de usuario

--- login.php
+++ login.php
@@ -1,3 +1,28 @@
+<?php
+
+if (!empty($_POST['email']) && !empty($_POST['password'])) {
+
+    $records = $conn->prepare('SELECT id, email, password FROM users WHERE email = :email');
+    $records-> bindParam(':email', $_POST['email']);
+    $records->execute();
+    $results = $records->fetch(PDO::FETCH_ASSOC);
+
+    $message = '';
+
+    if (count($results) > 0 && password_verify($_POST['password'])) {
+
+        $_SESSION['user_id'] = $results['id'];
+        header ("Location: /php-login");
+    } else {
+
+        $message = 'Sorry, those credentials do not match';
+    }
+}
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
